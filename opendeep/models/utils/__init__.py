ae918718e63132220ea751733ba1d19e5bc36db5
Markus Beissinger <mbeissinger@gmail.com>
2015-11-07 02:27:37 -0800

    fixed modifylayer imports

--- opendeep/models/utils/__init__.py
+++ opendeep/models/utils/__init__.py
@@ -3,3 +3,5 @@ from __future__ import division, absolute_import, print_function
 from .modify_layer import *
 from .activation import *
 from .pooling import *
+from .noise import *
+from .normalization import *
